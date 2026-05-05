2cc3102ed11f34edc3cd0f34a54031cd8c57ce5e
George <82040031+kantsiber@users.noreply.github.com>
2026-05-05 17:50:00 +0300

    add base_model

---
+++ models/EmoModel_base.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+
+
+class EmoModel(nn.Module):
+    def __init__(self, num_classes=4):
+        super().__init__()
+        self.models = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(256 * 2, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.35),
+            nn.Linear(512, num_classes)
+        )
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, 0, 0.01)
+                init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.models(x)
+        x = torch.cat([x.mean(dim=[-2, -1]), x.flatten(2).max(dim=-1).values],dim=1)
+        x = self.classifier(x)
+        return x
